1b4d5d3fe5a7f1711e61b1bec24b13471fe6c306
Justin M. Ramon <justinmramon@gmail.com>
2022-05-22 18:54:53 -0500

    commit tree 41cc0db93a0c2757524c290a239261d96899e348

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+// function findMatching(drivers, name){
+//     const newArrayOfDrivers = []
+//     for (const name of arrayOfDrivers)
+//     if (name === 'Bobby'){
+//         newArrayOfDrivers.push(name)
+//     }
+// }
+
+function findMatching(drivers, name){
+    const pikachu = drivers.filter(function(name2){
+        return name2 === name || name2 === name.toLowerCase()
+    })
+    return pikachu
+ }
+
+ function fuzzyMatch(drivers, firstLetter){
+    const fuzzyMatchedNames = drivers.filter(function(names){
+        return names.startsWith(firstLetter);
+    })
+    return fuzzyMatchedNames
+ }
+
+//  function matchName(drivers, moreNames){
+//     const whatAreWeLookingFor = drivers.filter(function(){
+//         return moreNames.name
+//     })
+//     return whatAreWeLookingFor
+//  }
+
+function matchName(drivers, theWeeknd){
+    const whatAreWeLookingFor = drivers.filter(function(nameProperties){
+        return nameProperties.name === theWeeknd
+    })
+    return whatAreWeLookingFor
+ }
